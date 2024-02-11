cf6b28c2b7b7466a71a6eee9c93e6e3d05056f52
Arthur Smith <arthur9smith7@gmail.com>
2024-02-11 20:28:52 +0100

    Implemented content of Home and CreateProduct screens.

--- app/src/main/java/com/way_torg/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/way_torg/myapplication/presentation/MainActivity.kt
@@ -3,15 +3,17 @@ package com.way_torg.myapplication.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.way_torg.myapplication.presentation.ui.theme.MyApplicationTheme
+import com.arkivanov.decompose.defaultComponentContext
+import com.way_torg.myapplication.presentation.root.DefaultRootComponent
+import com.way_torg.myapplication.presentation.root.RootContent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-
-            }
+            RootContent(
+                component = DefaultRootComponent(defaultComponentContext())
+            )
         }
     }
 }
