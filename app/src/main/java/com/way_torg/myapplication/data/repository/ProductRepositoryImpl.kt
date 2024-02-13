0b24dfe2e79b8450fa4d09f546ad7628daaf6e01
Arthur Smith <arthur9smith7@gmail.com>
2024-02-13 16:06:45 +0100

    Added new function.

--- app/src/main/java/com/way_torg/myapplication/data/repository/ProductRepositoryImpl.kt
+++ app/src/main/java/com/way_torg/myapplication/data/repository/ProductRepositoryImpl.kt
@@ -5,26 +5,31 @@ import com.google.firebase.storage.FirebaseStorage
 import com.way_torg.myapplication.data.mapper.toDto
 import com.way_torg.myapplication.domain.entity.Product
 import com.way_torg.myapplication.domain.repository.ProductRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
 
-class ProductRepositoryImpl(
+class ProductRepositoryImpl @Inject constructor(
     private val firestore: FirebaseFirestore,
     private val storage: FirebaseStorage
 ) : ProductRepository {
     private val ref = storage.reference
-    override suspend fun createProduct(product: Product): Result<Boolean> =
-        try {
-            // add pictures to storage
+    override suspend fun createProduct(product: Product): Result<Boolean> {
+        return Result.success(true)
+    }
 
-            // save product in db
-            val result = firestore.collection(PRODUCTS).add(product.toDto()).await()
+    override suspend fun addToBasket(product: Product): Result<Boolean> {
+        return Result.success(true)
+    }
 
-            Result.success(true)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
+    override fun getAllProducts(): Flow<List<Product>> {
+        return flow {  }
+    }
 
-
+    override fun getProductsFromBasket(): Flow<List<Product>> {
+        return flow {  }
+    }
 
     companion object {
         private const val PRODUCTS = "products"
--- app/src/main/java/com/way_torg/myapplication/domain/repository/ProductRepository.kt
+++ app/src/main/java/com/way_torg/myapplication/domain/repository/ProductRepository.kt
@@ -1,7 +1,12 @@
 package com.way_torg.myapplication.domain.repository
 
 import com.way_torg.myapplication.domain.entity.Product
+import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
     suspend fun createProduct(product: Product) : Result<Boolean>
+    suspend fun addToBasket(product: Product):Result<Boolean>
+    fun getAllProducts(): Flow<List<Product>>
+
+    fun getProductsFromBasket(): Flow<List<Product>>
 }
