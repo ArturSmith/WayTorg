078568ade6c9446f98b916123eb8b41bb46b25bf
Arthur Smith <arthur9smith7@gmail.com>
2024-02-13 16:00:31 +0100

    Created new Use cases.

--- app/src/main/java/com/way_torg/myapplication/domain/use_case/CreateProductUseCase.kt
+++ app/src/main/java/com/way_torg/myapplication/domain/use_case/CreateProductUseCase.kt
@@ -2,7 +2,8 @@ package com.way_torg.myapplication.domain.use_case
 
 import com.way_torg.myapplication.domain.entity.Product
 import com.way_torg.myapplication.domain.repository.ProductRepository
+import javax.inject.Inject
 
-data class CreateProductUseCase(private val repository: ProductRepository) {
+data class CreateProductUseCase @Inject constructor(private val repository: ProductRepository) {
     suspend operator fun invoke(product: Product) = repository.createProduct(product)
 }
