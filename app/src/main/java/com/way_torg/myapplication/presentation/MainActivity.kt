1f4f4d16adef0ce3db50ffce45c3fc728081c248
Arthur Smith <arthur9smith7@gmail.com>
2024-02-13 16:07:55 +0100

    Refactoring

--- app/src/main/java/com/way_torg/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/way_torg/myapplication/presentation/MainActivity.kt
@@ -6,13 +6,18 @@ import androidx.activity.compose.setContent
 import com.arkivanov.decompose.defaultComponentContext
 import com.way_torg.myapplication.presentation.root.DefaultRootComponent
 import com.way_torg.myapplication.presentation.root.RootContent
+import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var rootComponentFactory: DefaultRootComponent.Factory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        (applicationContext as WayTorg).applicationComponent.inject(this)
+        val root = rootComponentFactory.create(defaultComponentContext())
         setContent {
             RootContent(
-                component = DefaultRootComponent(defaultComponentContext())
+                component = root
             )
         }
     }
