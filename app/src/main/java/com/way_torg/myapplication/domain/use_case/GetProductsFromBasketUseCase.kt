4fced6343120a609535f2e0c4ec3108befa0b021
Arthur Smith <arthur9smith7@gmail.com>
2024-02-13 16:00:23 +0100

    Created new Use cases.

---
+++ app/src/main/java/com/way_torg/myapplication/domain/use_case/GetProductsFromBasketUseCase.kt
@@ -0,0 +1,8 @@
+package com.way_torg.myapplication.domain.use_case
+
+import com.way_torg.myapplication.domain.repository.ProductRepository
+import javax.inject.Inject
+
+class GetProductsFromBasketUseCase @Inject constructor(private val repository: ProductRepository) {
+    operator fun invoke() = repository.getProductsFromBasket()
+}
