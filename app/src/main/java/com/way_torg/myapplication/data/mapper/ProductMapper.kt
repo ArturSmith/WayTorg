a90a7f465abe0fc6e8c22843c7257fcd4b25c819
Arthur Smith <arthur9smith7@gmail.com>
2024-02-16 20:01:50 +0100

    Created new mappers.

--- app/src/main/java/com/way_torg/myapplication/data/mapper/ProductMapper.kt
+++ app/src/main/java/com/way_torg/myapplication/data/mapper/ProductMapper.kt
@@ -1,9 +1,11 @@
 package com.way_torg.myapplication.data.mapper
 
-import com.way_torg.myapplication.data.dto.ProductDto
+import com.way_torg.myapplication.data.local.model.ProductDbModel
+import com.way_torg.myapplication.data.network.dto.ProductDto
+import com.way_torg.myapplication.domain.entity.Category
 import com.way_torg.myapplication.domain.entity.Product
 
-fun Product.toDto() = ProductDto(id, name, category, description, count, price, discount, pictures, rating)
+fun Product.toModel() = ProductDbModel(id)
 
-fun ProductDto.toEntity() =
-    Product(id, name, category, description, count, price, discount, pictures, rating)
+fun Product.toDto() = ProductDto(id, name, category.toDto(), description, count, price, discount, pictures, rating)
+fun ProductDto.toEntity() = Product(id, name, category.toEntity(), description, count, price, discount, pictures, rating)
