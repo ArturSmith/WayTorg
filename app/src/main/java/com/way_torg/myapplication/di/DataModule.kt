25129bf32829f8e4580c114efabe2d8e6d144e80
Arthur Smith <arthur9smith7@gmail.com>
2024-02-16 19:52:51 +0100

    Implemented providing dao and database instances.

--- app/src/main/java/com/way_torg/myapplication/di/DataModule.kt
+++ app/src/main/java/com/way_torg/myapplication/di/DataModule.kt
@@ -1,10 +1,13 @@
 package com.way_torg.myapplication.di
 
+import android.content.Context
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
-import com.way_torg.myapplication.data.repository.FilterRepositoryImpl
+import com.way_torg.myapplication.data.local.db.Dao
+import com.way_torg.myapplication.data.local.db.WayTorgDatabase
+import com.way_torg.myapplication.data.repository.CategoryRepositoryImpl
 import com.way_torg.myapplication.data.repository.ProductRepositoryImpl
-import com.way_torg.myapplication.domain.repository.FilterRepository
+import com.way_torg.myapplication.domain.repository.CategoryRepository
 import com.way_torg.myapplication.domain.repository.ProductRepository
 import dagger.Binds
 import dagger.Module
@@ -17,7 +20,7 @@ interface DataModule {
     fun bindProductRepository(repository: ProductRepositoryImpl): ProductRepository
 
     @[ApplicationScope Binds]
-    fun bindFilterRepository(repository: FilterRepositoryImpl): FilterRepository
+    fun bindFilterRepository(repository: CategoryRepositoryImpl): CategoryRepository
 
 
     companion object {
@@ -25,7 +28,17 @@ interface DataModule {
         fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
 
         @[ApplicationScope Provides]
-        fun provideFirebaseStorage():FirebaseStorage = FirebaseStorage.getInstance()
+        fun provideFirebaseStorage(): FirebaseStorage = FirebaseStorage.getInstance()
+
+        @[ApplicationScope Provides]
+        fun provideWayTorgDatabase(context: Context): WayTorgDatabase {
+            return WayTorgDatabase.getInstance(context)
+        }
+
+        @[ApplicationScope Provides]
+        fun provideDao(wayTorgDatabase: WayTorgDatabase): Dao {
+            return wayTorgDatabase.dao()
+        }
     }
 
 }
