68ccf5cf3d02797e3474d198cf683d41e23fca42
Arthur Smith <arthur9smith7@gmail.com>
2024-02-16 19:06:20 +0100

    Implemented Parcelable interface and added default instance.

--- app/src/main/java/com/way_torg/myapplication/domain/entity/Category.kt
+++ app/src/main/java/com/way_torg/myapplication/domain/entity/Category.kt
@@ -1,6 +1,14 @@
 package com.way_torg.myapplication.domain.entity
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Category(
     val id: String,
     val name: String
-)
+):Parcelable {
+    companion object {
+        val defaultInstance = Category("","")
+    }
+}
