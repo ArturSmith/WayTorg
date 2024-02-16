d855f8b96b0fc0b4a788d4eedea28ad1c28456db
Arthur Smith <arthur9smith7@gmail.com>
2024-02-16 19:59:43 +0100

    Implemented parcelable interface and created default instance.

--- app/src/main/java/com/way_torg/myapplication/domain/entity/Product.kt
+++ app/src/main/java/com/way_torg/myapplication/domain/entity/Product.kt
@@ -1,13 +1,32 @@
 package com.way_torg.myapplication.domain.entity
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Product(
     val id: String,
     val name: String,
     val category: Category,
-    val description:String,
-    val count:Int,
-    val price:Double,
-    val discount:Double,
+    val description: String,
+    val count: Int,
+    val price: Double,
+    val discount: Double,
     val pictures: List<String>,
-    val rating:Double
-)
+    val rating: Double
+) :Parcelable{
+
+    companion object {
+        fun defaultInstance() = Product(
+            "",
+            "",
+            Category("", ""),
+            "",
+            0,
+            0.0,
+            0.0,
+            emptyList(),
+            0.0)
+    }
+
+}
