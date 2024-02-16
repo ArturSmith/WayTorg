0c4b74326129530b857f96cb18fbbc0c6b03dfbf
Arthur Smith <arthur9smith7@gmail.com>
2024-02-16 19:54:54 +0100

    Created new use cases.

---
+++ app/src/main/java/com/way_torg/myapplication/domain/use_case/GetAllCategoriesFromRemoteDbUseCase.kt
@@ -0,0 +1,8 @@
+package com.way_torg.myapplication.domain.use_case
+
+import com.way_torg.myapplication.domain.repository.CategoryRepository
+import javax.inject.Inject
+
+data class GetAllCategoriesFromRemoteDbUseCase @Inject constructor(private val repository: CategoryRepository) {
+    suspend operator fun invoke() = repository.getAllCategoriesFromRemoteDb()
+}
