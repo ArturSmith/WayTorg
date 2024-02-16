126e1bb830bb75647ec1bc54c1b0386854f36750
Arthur Smith <arthur9smith7@gmail.com>
2024-02-16 20:01:01 +0100

    Created db model for product class.

---
+++ app/src/main/java/com/way_torg/myapplication/data/local/model/ProductDbModel.kt
@@ -0,0 +1,10 @@
+package com.way_torg.myapplication.data.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.way_torg.myapplication.domain.entity.Category
+
+@Entity("products")
+data class ProductDbModel(
+    @PrimaryKey val id: String
+)
