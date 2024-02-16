f1266fbcb94739e009b574be295c7a480bd12cbd
Arthur Smith <arthur9smith7@gmail.com>
2024-02-16 19:48:06 +0100

    Added new argument to constructor of function.

--- app/src/main/java/com/way_torg/myapplication/domain/use_case/CreateProductUseCase.kt
+++ app/src/main/java/com/way_torg/myapplication/domain/use_case/CreateProductUseCase.kt
@@ -1,9 +1,10 @@
 package com.way_torg.myapplication.domain.use_case
 
+import android.net.Uri
 import com.way_torg.myapplication.domain.entity.Product
 import com.way_torg.myapplication.domain.repository.ProductRepository
 import javax.inject.Inject
 
 data class CreateProductUseCase @Inject constructor(private val repository: ProductRepository) {
-    suspend operator fun invoke(product: Product) = repository.createProduct(product)
+    suspend operator fun invoke(product: Product, uris:List<Uri>) = repository.createProduct(product, uris)
 }
