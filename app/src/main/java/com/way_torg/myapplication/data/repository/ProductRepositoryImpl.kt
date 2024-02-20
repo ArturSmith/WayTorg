ebcc1c7a10f1cf5e9cc6644fe16de593994afa00
Arthur Smith <arthur9smith7@gmail.com>
2024-02-20 13:48:03 +0100

    Implemented saving pictures into Storage and returning links.

--- app/src/main/java/com/way_torg/myapplication/data/repository/ProductRepositoryImpl.kt
+++ app/src/main/java/com/way_torg/myapplication/data/repository/ProductRepositoryImpl.kt
@@ -1,11 +1,12 @@
 package com.way_torg.myapplication.data.repository
 
 import android.net.Uri
+import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.toObject
 import com.google.firebase.storage.FirebaseStorage
-import com.way_torg.myapplication.data.local.db.Dao
+import com.way_torg.myapplication.data.local.db.AppDao
 import com.way_torg.myapplication.data.mapper.toDto
 import com.way_torg.myapplication.data.mapper.toEntity
 import com.way_torg.myapplication.data.mapper.toModel
@@ -16,24 +17,38 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
+import java.util.UUID
 import javax.inject.Inject
 
 class ProductRepositoryImpl @Inject constructor(
     private val firestore: FirebaseFirestore,
     private val storage: FirebaseStorage,
-    private val dao: Dao
+    private val appDao: AppDao
 ) : ProductRepository {
     private val ref = storage.reference
     override suspend fun createProduct(product: Product, uris: List<Uri>): Result<Boolean> {
         return try {
             if (uris.isNotEmpty()) {
-                val productRef = ref.child(product.id)
                 val paths = mutableListOf<String>()
                 uris.forEach { uri ->
-                    productRef.putFile(uri).await().metadata?.path?.let {
-                        paths.add(it)
-                    }
+                    val productRef = ref.child("products images/${UUID.randomUUID()}")
+                    productRef.putFile(uri)
+                        .continueWithTask { task ->
+                            if (!task.isSuccessful) {
+                                task.exception?.let {
+                                    throw it
+                                }
+                            }
+                            productRef.downloadUrl
+                        }.addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                task.result?.let {
+                                    paths.add(it.toString())
+                                }
+                            }
+                        }.await()
                 }
                 val newProduct = product.copy(pictures = paths).toDto()
                 firestore.collection(PRODUCTS).document().set(newProduct).await()
@@ -67,7 +82,7 @@ class ProductRepositoryImpl @Inject constructor(
 
     override suspend fun addProductToBasket(product: Product): Result<Boolean> {
         return try {
-            dao.addProduct(product.toModel())
+            appDao.addProduct(product.toModel())
             Result.success(true)
         } catch (e: Exception) {
             Result.failure(e)
@@ -76,7 +91,7 @@ class ProductRepositoryImpl @Inject constructor(
 
     override suspend fun deleteProductFromBasket(id: String): Result<Boolean> {
         return try {
-            dao.deleteProduct(id)
+            appDao.deleteProduct(id)
             Result.success(true)
         } catch (e: Exception) {
             Result.failure(e)
@@ -85,7 +100,7 @@ class ProductRepositoryImpl @Inject constructor(
 
     override fun getAllProducts(): Flow<List<Product>> = callbackFlow {
         val observer = firestore.collection(PRODUCTS).addSnapshotListener { value, error ->
-            if (error != null) return@addSnapshotListener
+            if (error != null || value?.documents?.isEmpty()==true) return@addSnapshotListener
 
             val products = value?.documents?.map {
                 it.toObject<ProductDto>()?.let { it.toEntity() } ?: Product.defaultInstance()
@@ -99,11 +114,12 @@ class ProductRepositoryImpl @Inject constructor(
         }
     }
 
-    override suspend fun getProductsFromBasket() = dao.getProducts().map {
-        it.id
-    }
+    override suspend fun getProductsFromBasket() =
+        appDao.getProducts().map {
+            it.id
+        }
 
-    override fun getCountOfProductsFromBasket() = dao.getCountOfProducts()
+    override fun getCountOfProductsFromBasket() = appDao.getCountOfProducts()
 
 
     private companion object {
