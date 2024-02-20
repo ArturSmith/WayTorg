0c475f275740c634241c7888b38c0427e957b8f0
Arthur Smith <arthur9smith7@gmail.com>
2024-02-20 13:22:07 +0100

    Created new database class.

---
+++ app/src/main/java/com/way_torg/myapplication/data/local/db/AppDatabase.kt
@@ -0,0 +1,40 @@
+package com.way_torg.myapplication.data.local.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.way_torg.myapplication.data.local.model.CategoryDbModel
+import com.way_torg.myapplication.data.local.model.ProductDbModel
+
+@Database(
+    entities = [ProductDbModel::class, CategoryDbModel::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase() : RoomDatabase() {
+    abstract fun appDao(): AppDao
+
+    companion object {
+        private const val NAME = "wayTorgDatabase"
+        private var INSTANCE: AppDatabase? = null
+        private val LOCK = Any()
+
+        fun getInstance(context: Context): AppDatabase {
+            INSTANCE?.let { return it }
+
+            synchronized(LOCK) {
+                INSTANCE?.let { return it }
+
+                val database = Room.databaseBuilder(
+                    context = context,
+                    klass = AppDatabase::class.java,
+                    name = NAME
+                ).build()
+
+                INSTANCE = database
+                return database
+            }
+        }
+    }
+}
