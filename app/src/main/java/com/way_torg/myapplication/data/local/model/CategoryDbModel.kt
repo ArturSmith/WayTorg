627235ff9d71a61a32277b1430ced1fbda112c69
Arthur Smith <arthur9smith7@gmail.com>
2024-02-20 13:22:33 +0100

    Refactoring.

--- app/src/main/java/com/way_torg/myapplication/data/local/model/CategoryDbModel.kt
+++ app/src/main/java/com/way_torg/myapplication/data/local/model/CategoryDbModel.kt
@@ -3,7 +3,7 @@ package com.way_torg.myapplication.data.local.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity("categories")
+@Entity(tableName = "categories")
 data class CategoryDbModel(
     @PrimaryKey val id: String,
     val name: String
