46631b549a8af93017822f49897ac020695537fe
Arthur Smith <arthur9smith7@gmail.com>
2024-02-23 13:53:43 +0100

    Refactoring.

--- app/src/main/java/com/way_torg/myapplication/domain/repository/ProductRepository.kt
+++ app/src/main/java/com/way_torg/myapplication/domain/repository/ProductRepository.kt
@@ -1,16 +1,18 @@
 package com.way_torg.myapplication.domain.repository
 
 import android.net.Uri
+import com.way_torg.myapplication.domain.entity.Category
 import com.way_torg.myapplication.domain.entity.Product
 import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
     fun getAllProducts(): Flow<List<Product>>
-    suspend fun createProduct(product: Product, uris:List<Uri>) : Result<Boolean>
-    suspend fun deleteProduct(id:String):Result<Boolean>
-    suspend fun editProduct(product: Product):Result<Boolean>
-    suspend fun addProductToBasket(product: Product):Result<Boolean>
-    suspend fun deleteProductFromBasket(id:String):Result<Boolean>
-    suspend fun getProductsFromBasket(): List<String>
-    fun getCountOfProductsFromBasket():Flow<Int>
+    suspend fun getProductsById(ids: List<String>): List<Product>
+    suspend fun createProduct(product: Product, uris: List<Uri>): Result<Boolean>
+    suspend fun deleteProduct(id: String): Result<Boolean>
+    suspend fun editProduct(product: Product): Result<Boolean>
+    suspend fun addProductToBasket(product: Product): Result<Boolean>
+    suspend fun deleteProductFromBasket(id: String): Result<Boolean>
+    fun getProductsFromBasket(): Flow<List<String>>
+    fun getCountOfProductsFromBasket(): Flow<Int>
 }
