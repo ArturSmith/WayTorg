4ff4e96d75203df85823880c4cccc72152ea6269
Arthur Smith <arthur9smith7@gmail.com>
2024-02-23 13:54:22 +0100

    Added new hosts.

--- app/src/main/java/com/way_torg/myapplication/presentation/root/RootComponent.kt
+++ app/src/main/java/com/way_torg/myapplication/presentation/root/RootComponent.kt
@@ -2,15 +2,19 @@ package com.way_torg.myapplication.presentation.root
 
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.value.Value
+import com.way_torg.myapplication.presentation.basket.BasketComponent
 import com.way_torg.myapplication.presentation.create_product.CreateProductComponent
+import com.way_torg.myapplication.presentation.details.DetailsComponent
 import com.way_torg.myapplication.presentation.home.HomeComponent
 
 interface RootComponent {
 
-    val stack : Value<ChildStack<*,Child>>
+    val stack: Value<ChildStack<*, Child>>
 
     sealed interface Child {
         data class CreateProduct(val component: CreateProductComponent) : Child
         data class Home(val component: HomeComponent) : Child
+        data class Details(val component: DetailsComponent) : Child
+        data class Basket(val component: BasketComponent) : Child
     }
 }
