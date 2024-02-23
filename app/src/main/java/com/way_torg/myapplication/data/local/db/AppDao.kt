4a3557889ae11c41bc334deec8f3740c652ef508
Arthur Smith <arthur9smith7@gmail.com>
2024-02-23 13:42:02 +0100

    Added new methods.

--- app/src/main/java/com/way_torg/myapplication/data/local/db/AppDao.kt
+++ app/src/main/java/com/way_torg/myapplication/data/local/db/AppDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.way_torg.myapplication.data.local.model.CategoryDbModel
+import com.way_torg.myapplication.data.local.model.CustomerInfoDbModel
 import com.way_torg.myapplication.data.local.model.ProductDbModel
 import kotlinx.coroutines.flow.Flow
 
@@ -12,16 +13,27 @@ import kotlinx.coroutines.flow.Flow
 interface AppDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addCategory(categoryDbModel: CategoryDbModel)
+
     @Query("DELETE FROM categories WHERE id=:id")
-    suspend fun deleteCategory(id:String)
+    suspend fun deleteCategory(id: String)
+
     @Query("SELECT * FROM categories")
-    fun getCategories() : Flow<List<CategoryDbModel>>
+    fun getCategories(): Flow<List<CategoryDbModel>>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addProduct(productDbModel: ProductDbModel)
+
     @Query("DELETE FROM products WHERE id=:id")
-    suspend fun deleteProduct(id:String)
-    @Query("SELECT * FROM products")
-    suspend fun getProducts():List<ProductDbModel>
+    suspend fun deleteProduct(id: String)
+
+    @Query("SELECT * FROM products ")
+    fun getProducts(): Flow<List<ProductDbModel>>
+
     @Query("SELECT COUNT(*) FROM products")
-    fun getCountOfProducts():Flow<Int>
+    fun getCountOfProducts(): Flow<Int>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addCustomer(customerInfoDbModel: CustomerInfoDbModel)
+    @Query("SELECT * FROM customers")
+    fun getCustomers():Flow<List<CustomerInfoDbModel>>
 }
