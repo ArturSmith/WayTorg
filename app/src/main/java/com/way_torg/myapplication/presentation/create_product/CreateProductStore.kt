bac1fb2832437501ca8438ab431d3d0d20d4be35
Arthur Smith <arthur9smith7@gmail.com>
2024-02-23 13:44:57 +0100

    Created method to validate fields.

--- app/src/main/java/com/way_torg/myapplication/presentation/create_product/CreateProductStore.kt
+++ app/src/main/java/com/way_torg/myapplication/presentation/create_product/CreateProductStore.kt
@@ -10,39 +10,61 @@ interface CreateProductStore :
     sealed interface State {
         data class Initial(
             val name: String,
-            val selectedCategory: Category,
+            val isNameError: Boolean,
+            val selectedCategory: Category?,
+            val isCategoryError: Boolean,
             val categoryName: String,
             val description: String,
+            val isDescriptionError: Boolean,
             val count: String,
+            val isCountError: Boolean,
             val price: String,
+            val isPriceError: Boolean,
             val discount: String,
             val pictures: List<Uri>,
-            val allCategories:List<Category>
-        ) :State
-        data object Loading:State
-        data object Error:State
-        data object Success:State
+            val allCategories: List<Category>
+        ) : State {
+            fun isAnyRequiredFieldEmpty() = name.isEmpty()
+                    || price.isEmpty()
+                    || description.isEmpty()
+                    || count.isEmpty()
+                    || categoryName.isEmpty()
+                    || selectedCategory == null
+                    || categoryName != selectedCategory.name
+
+            fun validateFields() = this.copy(
+                isNameError = name.isEmpty(),
+                isPriceError = price.isEmpty(),
+                isDescriptionError = description.isEmpty(),
+                isCountError = count.isEmpty(),
+                isCategoryError = selectedCategory == null
+                        || categoryName.isEmpty()
+                        || categoryName != selectedCategory.name
+            )
+
+        }
+
+        data object Loading : State
+        data object Error : State
+        data object Success : State
     }
 
 
     sealed interface Label {
-        data object OnClickBack : Label
         data object OnProductCreated : Label
     }
 
 
     sealed interface Intent {
-
         data class OnSetName(val name: String) : Intent
         data class OnCategorySelected(val category: Category) : Intent
         data class OnSetCount(val count: String) : Intent
         data class OnSetDescription(val description: String) : Intent
         data class OnSetPrice(val price: String) : Intent
         data class OnSetDiscount(val discount: String) : Intent
-        data class OnSetNewCategory(val categoryName:String):Intent
+        data class OnSetNewCategory(val categoryName: String) : Intent
         data class OnClickAddPictures(val pictures: List<Uri>) : Intent
         data class OnLongClickToPicture(val picture: Uri) : Intent
-        data object OnClickBack : Intent
         data object OnClickCreate : Intent
     }
 }
