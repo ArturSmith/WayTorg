9e4b8485b7fb521bc20e6d175228f23464f219c1
Arthur Smith <arthur9smith7@gmail.com>
2024-02-23 13:50:15 +0100

    Refactoring created separated files for extensions.

--- app/src/main/java/com/way_torg/myapplication/extensions/Extensions.kt
+++ app/src/main/java/com/way_torg/myapplication/extensions/Extensions.kt
@@ -19,44 +19,9 @@ fun ComponentContext.componentScope(): CoroutineScope = CoroutineScope(
 
 fun CreateProductStore.State.asInitial() = (this as CreateProductStore.State.Initial)
 
-fun String.toNewInt(): Int {
-    return if (this.isEmpty()) {
-        0
-    } else {
-        this.toInt()
-    }
-}
-
-fun String.toNewDouble(): Double {
-    return if (this.isEmpty()) {
-        0.0
-    } else {
-        this.toDouble()
-    }
-}
 
 
-fun List<Product>.filterByCategory(categories: List<Category>): List<Product> {
-    return if (categories.isEmpty()) this else this.filter { product -> categories.any { it.id == product.category.id } }
-}
 
-
-fun List<Category>.filterBySelectedCategories(selectedList: List<Category>): List<Category> {
-    return if (selectedList.isEmpty()) this else filter { cat -> !selectedList.any { it.id == cat.id } }
-}
-
-@Composable
-fun <T> Collection<T>.IfNotEmpty(predicate: @Composable () -> Unit) {
-    if (isNotEmpty()) {
-        predicate()
-    }
-}
-
-
-suspend fun String.getOrCreateCategory(
-    categories: List<Category>,
-    predicate: suspend (newCategory: String) -> Category
-) = categories.find { it.name == this } ?: predicate(this)
 
 
 
