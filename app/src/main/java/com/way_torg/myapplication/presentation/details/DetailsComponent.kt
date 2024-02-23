6f2b4a1d30a79ca6df7d8aa62c03ae7b9b88757a
Arthur Smith <arthur9smith7@gmail.com>
2024-02-23 13:49:12 +0100

    Added new methods.

---
+++ app/src/main/java/com/way_torg/myapplication/presentation/details/DetailsComponent.kt
@@ -0,0 +1,13 @@
+package com.way_torg.myapplication.presentation.details
+
+import com.way_torg.myapplication.domain.entity.Product
+import kotlinx.coroutines.flow.StateFlow
+
+interface DetailsComponent {
+    val model: StateFlow<DetailsStore.State>
+
+    fun onClickBasket()
+    fun onClickAddToBasket(product: Product)
+    fun onClickProduct(product: Product)
+    fun onClickBack()
+}
