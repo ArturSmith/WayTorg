93ec04b9dde0aa6386f330067a354279fe62d89f
Arthur Smith <arthur9smith7@gmail.com>
2024-02-23 13:51:15 +0100

    Refactoring.

--- app/src/main/java/com/way_torg/myapplication/presentation/home/HomeStore.kt
+++ app/src/main/java/com/way_torg/myapplication/presentation/home/HomeStore.kt
@@ -15,18 +15,9 @@ interface HomeStore : Store<HomeStore.Intent, HomeStore.State, HomeStore.Label> 
         val productsInBasket: Int
     )
 
-    sealed interface Label {
-        data class OnClickProduct(val product: Product) : Label
-        data object OnClickBasket : Label
-        data object OnClickChat : Label
-        data object OnClickCreateProduct : Label
-    }
+    sealed interface Label
 
     sealed interface Intent {
-        data class OnClickProduct(val product: Product) : Intent
-        data object OnClickBasket : Intent
-        data object OnClickChat : Intent
-        data object OnClickCreateProduct : Intent
         data class OnClickUnselectedCategory(val category: Category) : Intent
         data class OnClickSelectedCategory(val category: Category) : Intent
         data class OnClickAddToBasket(val product: Product) : Intent
