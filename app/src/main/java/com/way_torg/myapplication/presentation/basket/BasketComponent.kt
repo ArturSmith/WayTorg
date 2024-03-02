72eb17f05397937cb8c8572013eab77d47f3ecaa
Arthur Smith <arthur9smith7@gmail.com>
2024-03-02 21:21:45 +0100

    Added new intents.

--- app/src/main/java/com/way_torg/myapplication/presentation/basket/BasketComponent.kt
+++ app/src/main/java/com/way_torg/myapplication/presentation/basket/BasketComponent.kt
@@ -1,5 +1,6 @@
 package com.way_torg.myapplication.presentation.basket
 
+import com.way_torg.myapplication.domain.entity.CustomerInfo
 import com.way_torg.myapplication.domain.entity.Product
 import kotlinx.coroutines.flow.StateFlow
 
@@ -10,6 +11,11 @@ interface BasketComponent {
     fun decreaseQuantity(productId: String)
     fun onClickProduct(product: Product)
     fun delete(productId: String)
+    fun setCustomerName(name:String)
+    fun setCustomerAddress(address:String)
+    fun setCustomerContact(contact:String)
+    fun setCustomerMessage(message:String)
+    fun onClickCustomer(customerInfo: CustomerInfo)
     fun order()
     fun hideSheet()
     fun showSheet()
