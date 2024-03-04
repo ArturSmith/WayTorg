b450031a87916941fdd41a272b80f05a4716a51d
Arthur Smith <arthur9smith7@gmail.com>
2024-03-04 20:56:02 +0100

    Refactoring.

--- app/src/main/java/com/way_torg/myapplication/data/network/dto/OrderDto.kt
+++ app/src/main/java/com/way_torg/myapplication/data/network/dto/OrderDto.kt
@@ -1,18 +1,26 @@
 package com.way_torg.myapplication.data.network.dto
 
+import com.way_torg.myapplication.domain.entity.OrderStatus
+
 
 class OrderDto() {
+    var id: String = ""
     var products: List<ProductWrapperDto> = emptyList()
     var customerInfo: CustomerInfoDto = CustomerInfoDto()
     var orderDate: Long = 0
+    var status: OrderStatus = OrderStatus.UNPAID
 
     constructor(
+        id: String,
         products: List<ProductWrapperDto>,
         customerInfoDto: CustomerInfoDto,
-        orderDate: Long
+        orderDate: Long,
+        status: OrderStatus
     ) : this() {
+        this.id = id
         this.products = products
         this.customerInfo = customerInfoDto
         this.orderDate = orderDate
+        this.status = status
     }
 }
