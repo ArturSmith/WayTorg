8b50b49a7ca047383f8566ea889788f7d384fe81
Arthur Smith <arthur9smith7@gmail.com>
2024-03-04 20:54:17 +0100

    Refactoring.

--- app/src/main/java/com/way_torg/myapplication/domain/use_case/GetCustomersFromDbUseCase.kt
+++ app/src/main/java/com/way_torg/myapplication/domain/use_case/GetCustomersFromDbUseCase.kt
@@ -1,8 +1,10 @@
 package com.way_torg.myapplication.domain.use_case
 
 import com.way_torg.myapplication.domain.entity.Order
+import com.way_torg.myapplication.domain.repository.CustomerRepository
 import com.way_torg.myapplication.domain.repository.OrderRepository
+import javax.inject.Inject
 
-class GetCustomersFromDbUseCase(private val repository: OrderRepository) {
+class GetCustomersFromDbUseCase @Inject constructor(private val repository: CustomerRepository) {
      operator fun invoke() = repository.getCustomersFromDb()
 }
