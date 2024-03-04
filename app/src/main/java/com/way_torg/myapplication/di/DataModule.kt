dbd68402408358b7f2bf686bff479095aa2a8863
Arthur Smith <arthur9smith7@gmail.com>
2024-03-04 20:49:58 +0100

    Added new dependencies to the DI model.

--- app/src/main/java/com/way_torg/myapplication/di/DataModule.kt
+++ app/src/main/java/com/way_torg/myapplication/di/DataModule.kt
@@ -1,13 +1,20 @@
 package com.way_torg.myapplication.di
 
 import android.content.Context
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.way_torg.myapplication.data.local.db.AppDao
 import com.way_torg.myapplication.data.local.db.AppDatabase
+import com.way_torg.myapplication.data.repository.AuthRepositoryImpl
 import com.way_torg.myapplication.data.repository.CategoryRepositoryImpl
+import com.way_torg.myapplication.data.repository.CustomerRepositoryImpl
+import com.way_torg.myapplication.data.repository.OrderRepositoryImpl
 import com.way_torg.myapplication.data.repository.ProductRepositoryImpl
+import com.way_torg.myapplication.domain.repository.AuthRepository
 import com.way_torg.myapplication.domain.repository.CategoryRepository
+import com.way_torg.myapplication.domain.repository.CustomerRepository
+import com.way_torg.myapplication.domain.repository.OrderRepository
 import com.way_torg.myapplication.domain.repository.ProductRepository
 import dagger.Binds
 import dagger.Module
@@ -22,6 +29,15 @@ interface DataModule {
     @[ApplicationScope Binds]
     fun bindFilterRepository(repository: CategoryRepositoryImpl): CategoryRepository
 
+    @[ApplicationScope Binds]
+    fun bindsOrderRepository(repository: OrderRepositoryImpl): OrderRepository
+
+    @[ApplicationScope Binds]
+    fun bindsCustomerRepository(repository: CustomerRepositoryImpl): CustomerRepository
+
+    @[ApplicationScope Binds]
+    fun bindsAuthRepository(repository: AuthRepositoryImpl): AuthRepository
+
 
     companion object {
         @[ApplicationScope Provides]
@@ -29,6 +45,9 @@ interface DataModule {
 
         @[ApplicationScope Provides]
         fun provideFirebaseStorage(): FirebaseStorage = FirebaseStorage.getInstance()
+
+        @[ApplicationScope Provides]
+        fun providesFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
 
         @[ApplicationScope Provides]
         fun provideAppDatabase(context: Context): AppDatabase {
